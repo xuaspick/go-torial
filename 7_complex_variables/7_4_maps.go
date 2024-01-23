f44a0c0b367672b5d0a64f5e4e619de2c9a81d06
LX <asdasd@gmail.com>
2024-01-22 22:41:22 -0300

    cierre maps

--- 7_complex_variables/7_4_maps.go
+++ 7_complex_variables/7_4_maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type coordenadas struct {
 	Lat, Lon float64
@@ -20,4 +23,26 @@ func introToMaps() {
 		37.42202, -122.08408,
 	}
 	fmt.Printf("The latitude of Google is: %v\n", m["Google"].Lat)
+}
+
+func mutatingMaps() {
+	fmt.Printf("\n## Mutating Maps in Go ##\n")
+	m := make(map[string]int)
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+	v, ok := m["Answer"] //? ok is a bool that indicates if the key was present in the map, v is the default value of the type
+	fmt.Println("The value:", v, "Present?", ok)
+}
+
+func WordCount(s string) map[string]int {
+	counterMap := make(map[string]int)
+	words := strings.Fields(s)
+	for _, word := range words {
+		counterMap[word]++
+	}
+	return counterMap
 }
--- 7_complex_variables/main.go
+++ 7_complex_variables/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// introToPointers()
 	// introToStructs()
@@ -15,4 +17,8 @@ func main() {
 	// rangeForSlice()
 	rangeForSlice2()
 	introToMaps()
+	mutatingMaps()
+
+	fmt.Println(WordCount("I ate a donut. Then I ate another donut."))
+	fmt.Println(WordCount("The quick brown fox jumped over the lazy dog."))
 }
